8aac70197bca67b9d4cca619c2d1d2249ae1d80c
ashtotakoe <egormartynov12@gmail.com>
2023-10-21 21:40:04 +0400

    feat(header): add sorting options component

--- src/app/core/header/header.module.ts
+++ src/app/core/header/header.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
 import { HeaderComponent } from './components/header/header.component'
+import { SortingOptionsComponent } from './sorting-options/sorting-options.component'
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, SortingOptionsComponent],
   imports: [CommonModule],
   exports: [HeaderComponent, CommonModule],
 })
