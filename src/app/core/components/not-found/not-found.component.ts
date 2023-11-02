d4c57c7399cb8550c63736f9b0259e5688f6626c
ashtotakoe <egormartynov12@gmail.com>
2023-11-02 21:19:17 +0400

    feat(root): add not fofound page

--- src/app/core/components/not-found/not-found.component.ts
+++ src/app/core/components/not-found/not-found.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'yt-not-found',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, RouterModule],
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
 })
