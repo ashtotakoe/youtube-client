b3be13b5a6078e598739b8f7f0a708b73e7f01d7
ashtotakoe <egormartynov12@gmail.com>
2023-12-13 19:29:27 +0400

    chore: add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { RouterModule } from '@angular/router'
+import { StoreModule } from '@ngrx/store'
 
 import { AppComponent } from './app.component'
 import { appRoutes } from './app.routes'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes), BrowserAnimationsModule],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), BrowserAnimationsModule, StoreModule.forRoot({}, {})],
   providers: [],
   bootstrap: [AppComponent],
 })
