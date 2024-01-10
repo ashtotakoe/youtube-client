77377a2f32f955da2c17a06e2ba62b1ce3489eb3
ashtotakoe <EgorMartynov12@gmail.com>
2024-01-10 17:02:16 +0400

    refactor: minor changes

--- src/app/home/components/groups-list/groups-list.component.ts
+++ src/app/home/components/groups-list/groups-list.component.ts
@@ -5,7 +5,6 @@ import { HomeFacade } from '../../home-store/services/home.facade'
 import type { Group } from '../../models/group.model'
 import { CreateGroupDialogFormComponent } from '../create-group-dialog-form/create-group-dialog-form.component'
 import { CountdownNames } from 'src/app/core/enums/countdown-names.enum'
-import { ProfileFacade } from 'src/app/profile/profile-store/services/profile.facade'
 
 @Component({
   selector: 'cn-group-list',
@@ -16,12 +15,11 @@ import { ProfileFacade } from 'src/app/profile/profile-store/services/profile.fa
 export class GroupListComponent implements OnInit {
   public groups$ = this.homeFacade.groups$
   public isLoading$ = this.homeFacade.isLoading$
-  public profileData$ = this.profileFacade.profileData$
 
   public countDownName = CountdownNames.RefreshGroupList
+
   constructor(
     private homeFacade: HomeFacade,
-    private profileFacade: ProfileFacade,
     private dialog: MatDialog,
   ) {}
 
--- src/app/home/components/users-list/users-list.component.ts
+++ src/app/home/components/users-list/users-list.component.ts
@@ -2,7 +2,6 @@ import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core'
 
 import { HomeFacade } from '../../home-store/services/home.facade'
 import { CountdownNames } from 'src/app/core/enums/countdown-names.enum'
-import { ProfileFacade } from 'src/app/profile/profile-store/services/profile.facade'
 
 @Component({
   selector: 'cn-users-list',
@@ -13,20 +12,16 @@ import { ProfileFacade } from 'src/app/profile/profile-store/services/profile.fa
 export class UsersListComponent implements OnInit {
   public users$ = this.homeFacade.users$
   public isLoading$ = this.homeFacade.isLoading$
-  private profileData$ = this.profileFacade.profileData$
 
   public countdownName = CountdownNames.RefreshUserList
 
-  constructor(
-    private homeFacade: HomeFacade,
-    private profileFacade: ProfileFacade,
-  ) {}
-
-  public ngOnInit(): void {
-    this.homeFacade.loadUsers({ isCashed: true })
-  }
+  constructor(private homeFacade: HomeFacade) {}
 
   public refreshUsers(): void {
     this.homeFacade.loadUsers({ isCashed: false })
+  }
+
+  public ngOnInit(): void {
+    this.homeFacade.loadUsers({ isCashed: true })
   }
 }
